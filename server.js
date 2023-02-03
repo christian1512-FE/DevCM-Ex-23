fb1ff93352c5db63323c5734765f9b266ba5eedc
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2023-02-03 12:12:32 -0600

    testing ports

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const fs = require('fs');   //Used to store & retrieve notes
 
 const app = express(); //Invoke express variable & call it app 
 
-const PORT = 3000;
+// const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 
 const util = require('util');
 
